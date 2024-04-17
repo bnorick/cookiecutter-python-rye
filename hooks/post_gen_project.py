54dfd497cd952fc3c13b3f6812550c636fa2dca5
Brandon Norick <b.norick@gmail.com>
2024-04-17 11:50:56 -0700

    Improves virtual projects

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,71 @@
 #!/usr/bin/env python
-import os
 import pathlib
 import shutil
 
 PROJECT_DIRECTORY = pathlib.Path.cwd().expanduser().resolve()
 
 
-def delete(relpath):
-    target = PROJECT_DIRECTORY / relpath
-    rm(target)
+def _keep_recursive(root, keep):
+    if root in keep:
+        return
+
+    for p in root.iterdir():
+        if p.is_dir():
+            _keep_recursive(p, keep)
+        elif p not in keep:
+            p.unlink()
 
 
-def rm(target: pathlib.Path):
+def _remove_empty_dirs(root):
+    dir_iter = root.iterdir()
+    try:
+        removed_any = False
+        first = next(dir_iter)
+        if first.is_dir():
+            removed_any = _remove_empty_dirs(first)
+        for p in dir_iter:
+            if p.is_dir():
+                removed_any = _remove_empty_dirs(p)
+
+        # reconsider the current directory now that it's children have
+        # been processed
+        if removed_any:
+            _remove_empty_dirs(root)
+    except StopIteration:
+        root.rmdir()
+        return True
+
+
+def filter_directory(directory, *keep_relpaths, remove_empty=True):
+    keep = set()
+    for relpath in keep_relpaths:
+        relpath = pathlib.Path(relpath)
+        if relpath.is_absolute():
+            raise ValueError(f"Invalid argument, {relpath} is an absolute path.")
+        keep.add(directory / relpath)
+
+    _keep_recursive(directory, keep)
+    if remove_empty:
+        _remove_empty_dirs(directory)
+
+
+def delete(*, abspath=None, relpath=None):
+    target = None
+    if abspath:
+        target = pathlib.Path(abspath)
+
+    if relpath:
+        if target:
+            raise ValueError(f"Invalid arguments, both abspath and relpath were passed.")
+        target = PROJECT_DIRECTORY / relpath
+
+    if not target:
+        raise ValueError(f"Invalid arguments, abspath or relpath must be passed.")
+
+    _rm_any(target)
+
+
+def _rm_any(target: pathlib.Path):
     if target.is_file():
         target.unlink()
     elif target.is_dir():
@@ -21,16 +75,35 @@ def rm(target: pathlib.Path):
 
 
 if __name__ == "__main__":
-
-    if "True" == "{{ cookiecutter.virtual }}":
-        keep = {".gitignore", ".python-version", "LICENSE", "pyproject.toml", "README.md"}
-        for p in PROJECT_DIRECTORY.iterdir():
-            if p.name not in keep:
-                rm(p)
+    virtual = "yes" in "{{ cookiecutter.virtual|lower }}"
+    monorepo = "monorepo" in "{{ cookiecutter.virtual|lower }}"
+    exploratory = "exploratory" in "{{ cookiecutter.virtual|lower }}"
+    with_cli = "{{ cookiecutter.command_line_interface|lower }}" != "none"
+    makefile_path = PROJECT_DIRECTORY / "Makefile"
+    if virtual:
+        to_keep = [
+            ".gitignore",
+            ".python-version",
+            "LICENSE",
+            "pyproject.toml",
+            "README.md",
+            "Makefile.virtual",
+            "bin/checksum",
+            "bin/verchew",
+            ".verchew.ini",
+        ]
+        if with_cli:
+            to_keep.append("bin/{{ cookiecutter.package_name }}")
+        if exploratory:
+            to_keep.append("notebooks")
+        filter_directory(PROJECT_DIRECTORY, *to_keep)
+        (PROJECT_DIRECTORY / "Makefile.virtual").rename(makefile_path)
     else:
-        if "no" in "{{ cookiecutter.command_line_interface|lower }}":
-            cli_file = os.path.join("src", "{{ cookiecutter.package_name }}", "cli.py")
-            delete(cli_file)
+        # delete files only used in virtual projects
+        delete(relpath="bin/{{ cookiecutter.package_name }}")
+        delete(relpath="Makefile.virtual")
+        if not with_cli:
+            delete(relpath="src/{{ cookiecutter.package_name }}/cli.py")
 
-    if "Not open source" == "{{ cookiecutter.open_source_license }}":
-        delete("LICENSE")
+    if "{{ cookiecutter.open_source_license }}" == "Not open source":
+        delete(relpath="LICENSE")
